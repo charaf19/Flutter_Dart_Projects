78f509f82d7ce6c53086fd85a41473279fb2cb43
charaf hamidi <34777843+charaf19@users.noreply.github.com>
2022-09-19 15:03:52 +0200

    showing pictures and titles from app data file

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import '../app_data.dart';
+import '../widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('دليل سياحي'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 7 / 8,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10),
+        children: Categories_data.map((categoryData) =>
+            CategoryItem(categoryData.imageUrl, categoryData.title)).toList(),
+      ),
+    );
+  }
+}
