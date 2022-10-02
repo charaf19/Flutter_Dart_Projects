ec0eb355f6e1455bb2ee541e219badee4682aff3
charaf hamidi <34777843+charaf19@users.noreply.github.com>
2022-10-02 16:47:11 +0200

    main screen

--- minimal_app/lib/pages/welcome.dart
+++ minimal_app/lib/pages/welcome.dart
@@ -1,10 +1,22 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(children: [
+        Positioned(
+          child: Image.asset(""),
+        ),
+        Positioned(
+          child: Image.asset(""),
+        ),
+      ]),
+    );
   }
 }
